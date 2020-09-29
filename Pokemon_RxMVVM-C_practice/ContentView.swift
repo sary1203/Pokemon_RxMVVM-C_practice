6e5154444cb4722a40c7ee3ad5e3c215647b1e66
Randy Flores <floresrandyjr@gmail.com>
2020-09-29 09:16:48 +0800

    Initial Commit

---
+++ Pokemon_RxMVVM-C_practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokemon_RxMVVM-C_practice
+//
+//  Created by Randy Flores on 9/29/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
